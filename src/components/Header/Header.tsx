5d8922eb86c27168c70bd61a0d6fdfb04f64d35d
Dušan Janošević <dusan.janosevic123@gmail.com>
2023-03-25 14:53:41 +0100

    finished login

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,40 +1,66 @@
-import { Box, Button, Divider, Flex, Link, Text, useDisclosure } from '@chakra-ui/react'
-import { GiCommercialAirplane } from 'react-icons/gi'
-import React from 'react'
-import { CreateFlight } from '../Flight/CreateFlight'
+import {
+  Box,
+  Button,
+  Divider,
+  Flex,
+  Link,
+  Text,
+  useDisclosure,
+} from "@chakra-ui/react";
+import { GiCommercialAirplane } from "react-icons/gi";
+import React from "react";
+import { CreateFlight } from "../Flight/CreateFlight";
+import { LoginForm } from "../Auth/LoginForm";
 
 export const Header = () => {
+  const {
+    isOpen: isOpenAdd,
+    onOpen: onOpenAdd,
+    onClose: onCloseAdd,
+  } = useDisclosure();
+  const {
+    isOpen: isOpenLogin,
+    onOpen: onOpenLogin,
+    onClose: onCloseLogin,
+  } = useDisclosure();
 
-    const { isOpen: isOpenAdd, onOpen: onOpenAdd, onClose: onCloseAdd } = useDisclosure()
-
-    return (
-        <>
-            <Box width='100%' bg={'#003b95'} p={'10px 25px'}>
-                <Flex
-
-                    w={'100%'}
-                    h={'40px'}
-                    alignItems={'center'}
-                    justifyContent='space-between'
-                >
-                    <Text color={'white'} fontWeight='700'>Flight.com</Text>
-                    <Flex gap='15px'>
-                        <Link color={'white'}>Login</Link>
-                        <Link color={'white'}>Register</Link>
-                    </Flex>
-
-                </Flex>
-                <Divider></Divider>
-                <Flex m={'20px 0'}>
-                    <Button onClick={onOpenAdd}>
-                        <GiCommercialAirplane></GiCommercialAirplane>
-                        Create flight
-                    </Button>
-                </Flex>
-            </Box>
-            <CreateFlight isOpen={isOpenAdd} onOpen={onOpenAdd} onClose={onCloseAdd}></CreateFlight>
-        </>
-
-
-    )
-}
+  return (
+    <>
+      <Box width="100%" bg={"#003b95"} p={"10px 25px"}>
+        <Flex
+          w={"100%"}
+          h={"40px"}
+          alignItems={"center"}
+          justifyContent="space-between"
+        >
+          <Text color={"white"} fontWeight="700">
+            Flight.com
+          </Text>
+          <Flex gap="15px">
+            <Link onClick={onOpenLogin} color={"white"}>
+              Login
+            </Link>
+            <Link color={"white"}>Register</Link>
+          </Flex>
+        </Flex>
+        <Divider></Divider>
+        <Flex m={"20px 0"}>
+          <Button onClick={onOpenAdd}>
+            <GiCommercialAirplane></GiCommercialAirplane>
+            Create flight
+          </Button>
+        </Flex>
+      </Box>
+      <CreateFlight
+        isOpen={isOpenAdd}
+        onOpen={onOpenAdd}
+        onClose={onCloseAdd}
+      ></CreateFlight>
+      <LoginForm
+        isOpen={isOpenLogin}
+        onOpen={onOpenLogin}
+        onClose={onCloseLogin}
+      ></LoginForm>
+    </>
+  );
+};
