45dd63ff5443589d567c21e5ce390311288261d2
sara1404 <sara14042000@gmail.com>
2023-03-28 20:09:23 +0200

    validation for create flight form added

--- src/components/Flight/CreateFlight.tsx
+++ src/components/Flight/CreateFlight.tsx
@@ -1,8 +1,10 @@
-import { Button, Input, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay } from '@chakra-ui/react'
+import { Button, Input, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Text } from '@chakra-ui/react'
+import { yupResolver } from '@hookform/resolvers/yup'
 import React from 'react'
 import { SubmitHandler, useForm } from 'react-hook-form'
 import { useApplicationStore } from '../../store/application.store'
 import { Flight } from '../../store/flight-store/types/flight.type'
+import { CREATE_FLIGHT_VALIDATION_SCHEMA } from '../../utils/flight.constants'
 
 interface Props {
     isOpen: boolean
@@ -24,7 +26,9 @@ export const CreateFlight = ({ isOpen, onOpen, onClose }: Props) => {
     const createFlight = useApplicationStore(state => state.createFlight)
     const getFlights = useApplicationStore(state => state.getFlights)
 
-    const { register, handleSubmit, watch, formState: { errors } } = useForm<Inputs>();
+    const { register, handleSubmit, watch, formState: { errors } } = useForm<Inputs>({
+        resolver: yupResolver(CREATE_FLIGHT_VALIDATION_SCHEMA)
+    });
     const onSubmit: SubmitHandler<Inputs> = async data => {
         await createFlight(data)
         onClose()
@@ -46,10 +50,15 @@ export const CreateFlight = ({ isOpen, onOpen, onClose }: Props) => {
                 <ModalCloseButton />
                 <ModalBody width='100%' display='flex' flexDirection='column' alignItems='center'>
                     <Input {...register("date", { required: true })} placeholder='Flight date' type='datetime-local' mb={'15px'} width='80%'></Input>
+                    {errors.date && <Text color={'red'} mt='-10px'>Date is required</Text>}
                     <Input {...register("departure", { required: true })} placeholder='Flight departure' mb={'15px'} width='80%'></Input>
+                    {errors.departure && <Text color={'red'} mt='-10px'>Departure is required</Text>}
                     <Input {...register("destination", { required: true })} placeholder='Flight destination' mb={'15px'} width='80%'></Input>
-                    <Input {...register("seats", { required: true })} placeholder='Maximum seats' type='number' mb={'15px'} width='80%'></Input>
-                    <Input {...register("price", { required: true })} placeholder='Price' type='number' mb={'15px'} width='80%'></Input>
+                    {errors.destination && <Text color={'red'} mt='-10px'>Destination is required</Text>}
+                    <Input {...register("seats", { required: true, valueAsNumber: true })} placeholder='Maximum seats' type='number' mb={'15px'} width='80%'></Input>
+                    {errors.seats && <Text color={'red'} mt='-10px'>{errors.seats.message}</Text>}
+                    <Input {...register("price", { required: true, valueAsNumber: true })} placeholder='Price' type='number' mb={'15px'} width='80%'></Input>
+                    {errors.price && <Text color={'red'} mt='-10px'>{errors.price.message}</Text>}
                 </ModalBody>
                 <ModalFooter width='100%' display='flex' justifyContent='center'>
                     <Button bg={'#003b95'} color={'#fff'} _hover={{ bg: '#136ed1' }} width='80%' onClick={handleSubmit(onSubmit)}>Create flight</Button>
---
+++ src/utils/flight.constants.ts
@@ -0,0 +1,9 @@
+import * as yup from "yup"
+
+export const CREATE_FLIGHT_VALIDATION_SCHEMA = yup.object({
+    date: yup.date().required(),
+    departure: yup.string().required(),
+    destination: yup.string().required(),
+    seats: yup.number().typeError("Seats number is required!").min(1).required(),
+    price: yup.number().typeError("Price is required!").min(1).required()
+})
