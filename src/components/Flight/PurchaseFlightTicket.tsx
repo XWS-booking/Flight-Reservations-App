84e47fef5cef0753f1d4177bfa532a9649592bcb
sara1404 <sara14042000@gmail.com>
2023-03-31 19:08:55 +0200

    fix

--- src/components/Flight/PurchaseFlightTicket.tsx
+++ src/components/Flight/PurchaseFlightTicket.tsx
@@ -1,6 +1,6 @@
 import { Button, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Text } from '@chakra-ui/react'
 import { format } from 'date-fns'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useApplicationStore } from '../../store/application.store'
 import { Flight } from '../../store/flight-store/types/flight.type'
 import { Counter } from '../Counter/Counter'
@@ -10,30 +10,34 @@ interface Props {
     onOpen: () => void
     onClose: () => void
     flight: Flight
-    handlePurchase: (flightId: string, quantity: number) => void
+    handlePurchase: (flightId: string, quantity: number) => Promise<void>
 }
 
 export const PurchaseFlightTicket = ({ isOpen, onOpen, onClose, flight, handlePurchase }: Props) => {
 
 
-    const [quantity, setQuantity] = useState<number>(1)
+    const [quantity, setQuantity] = useState<number>(0)
+
+    const handleClick = async () => {
+        await handlePurchase(flight.id, quantity)
+        setQuantity(0)
+    }
 
     return (
         <Modal isOpen={isOpen} onClose={onClose}>
             <ModalOverlay />
             <ModalContent>
                 <ModalHeader textAlign={'center'}>Purchase flight ticket</ModalHeader>
-                <ModalCloseButton />
+                <ModalCloseButton onClick={() => setQuantity(0)} />
                 <ModalBody width='100%' display='flex' flexDirection='column' alignItems='center' gap={'10px'}>
                     <Text fontSize={18} fontWeight={500}>{flight.departure} - {flight.destination}</Text>
                     <Text fontSize={16}>{format(new Date(flight.date), 'dd-MM-yyyy HH:MM').toString()}</Text>
-                    <Counter quantity={quantity} setQuantity={setQuantity} maxQuantity={flight.freeSeats ?? 0}></Counter>
+                    <Counter quantity={quantity} setQuantity={setQuantity} maxQuantity={flight.freeSeats}></Counter>
                 </ModalBody>
                 <ModalFooter width='100%' display='flex' justifyContent='center'>
-                    <Button bg={'#003b95'} color={'#fff'} _hover={{ bg: '#136ed1' }} width='80%' onClick={() => handlePurchase(flight.id, quantity)}>Purchase ticket</Button>
+                    <Button bg={'#003b95'} color={'#fff'} _hover={{ bg: '#136ed1' }} width='80%' onClick={() => handleClick()}>Purchase ticket</Button>
                 </ModalFooter>
             </ModalContent>
         </Modal >
-
     )
 }
